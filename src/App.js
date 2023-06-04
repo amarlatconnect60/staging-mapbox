86e50048667640d40580a3a885eaeb5a86a8d8c7
Amar Yasier Kamarudin <amar@latconnect60.com>
2023-06-05 00:05:44 +0800

    adjust style

--- src/App.js
+++ src/App.js
@@ -74,10 +74,6 @@ const vectorTilesets = [
     center: [100.392, 6.016],
     zoom: 13.11,
   },
-  // {
-  //   name: 'ALL_SBB_POLYGON-7gaa7g',
-  //   id: 'ludfi.99uvhjbb',
-  // },
 ]
 
 const App = () => {
@@ -88,7 +84,7 @@ const App = () => {
   useEffect(() => {
     const mapInstance = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/satellite-streets-v11',
+      style: 'mapbox://styles/mapbox/dark-v11',
       center: vectorTilesets[0].center,
       zoom: vectorTilesets[0].zoom,
     })
@@ -156,7 +152,7 @@ const App = () => {
         'source-layer': 'ALL_SBB_POLYGON-7gaa7g',
         paint: {
           'line-color': 'black', // black line
-          'line-width': 2, // adjust to the desired thickness
+          'line-width': 3, // adjust to the desired thickness
         },
       })
 
@@ -287,142 +283,3 @@ const App = () => {
 }
 
 export default App
-
-//import React, { useEffect, useRef, useState } from 'react'
-// import mapboxgl from 'mapbox-gl'
-// import 'mapbox-gl/dist/mapbox-gl.css' // Import mapbox-gl CSS
-
-// mapboxgl.accessToken =
-//   'pk.eyJ1IjoibHVkZmkiLCJhIjoiY2xoZWpwemM1MWxzbjN0b2FyaW5qdXJzNCJ9.c-SshhzMhRvnuUqku2LKEg' // Use your Mapbox token here
-
-// const App = () => {
-//   const mapContainerRef = useRef(null)
-//   const popupRef = useRef(new mapboxgl.Popup({ offset: 15 }))
-//   const [mapState, setMapState] = useState({
-//     tilesetId: 'SBB_SawahSempadan-b5zxxf',
-//     center: [101.212, 3.456],
-//     zoom: 12.47,
-//   })
-//   const mapRef = useRef(null)
-
-//   const tilesets = [
-// { id: 'SBB_SawahSempadan-b5zxxf', center: [101.212, 3.456], zoom: 12.47 },
-// { id: 'SBB_SungaiBesar-5ivn2i', center: [100.996, 3.689], zoom: 12.38 },
-// { id: 'SBB_SungaiManik-1h1g7j', center: [101.077, 4.078], zoom: 14.01 },
-// { id: 'SBB_LabuKubong-6jf9ep', center: [101.058, 4.105], zoom: 12.09 },
-// { id: 'SBB_LamborKiri-97sftn', center: [100.888, 4.262], zoom: 14.43 },
-// { id: 'SBB_Kerian-b5je8f', center: [100.473, 5.078], zoom: 12.21 },
-// { id: 'SBB_SungaiDua-5dp8bq', center: [100.425, 5.466], zoom: 13.49 },
-// { id: 'SBB_TikamBatu-dhrwfs', center: [100.426, 5.569], zoom: 14.33 },
-// { id: 'SBB_SelarongBatang-7oosb4', center: [100.392, 6.016], zoom: 13.11 },
-// ]
-
-//   const changeTileset = (tilesetId, center, zoom) => {
-//     setMapState({ tilesetId, center, zoom })
-//   }
-
-//   useEffect(() => {
-//     mapRef.current = new mapboxgl.Map({
-//       container: mapContainerRef.current,
-//       style: 'mapbox://styles/mapbox/streets-v11',
-//       center: mapState.center,
-//       zoom: mapState.zoom,
-//     })
-
-//     mapRef.current.on('load', () => {
-//       mapRef.current.addSource('your-vector-source', {
-//         type: 'vector',
-//         url: `mapbox://${mapState.tilesetId}`,
-//       })
-
-//       mapRef.current.addLayer({
-//         id: 'your-vector-layer',
-//         type: 'fill',
-//         source: 'your-vector-source',
-//         'source-layer': mapState.tilesetId,
-//         paint: {
-//           'fill-color': [
-//             'match',
-//             ['get', 'gridcode'],
-//             1,
-//             'red',
-//             2,
-//             'orange',
-//             3,
-//             'yellow',
-//             4,
-//             'green',
-//             'black',
-//           ],
-//           'fill-opacity': 0.6,
-//         },
-//       })
-
-//       mapRef.current.on('mousemove', 'your-vector-layer', e => {
-//         if (e.features.length) {
-//           const feature = e.features[0]
-//           popupRef.current
-//             .setLngLat(e.lngLat)
-//             .setHTML(`SBB_SawahSempadan-b5zxxf`)
-//             .addTo(mapRef.current)
-//         }
-//       })
-
-//       mapRef.current.on('mouseleave', 'your-vector-layer', () => {
-//         popupRef.current.remove()
-//       })
-
-//       mapRef.current.on('click', 'your-vector-layer', e => {
-//         if (e.features.length) {
-//           const feature = e.features[0]
-//           const popupContent = `
-//             SBB_SawahSempadan-b5zxxf<br/>
-//             id (${feature.id})<br/>
-//             gridcode# (${feature.properties.gridcode})<br/>
-//             Id# (${feature.properties.Id})<br/>
-//             Shape_Area# (${feature.properties.Shape_Area})<br/>
-//             Shape_Leng# (${feature.properties.Shape_Leng})
-//           `
-//           new mapboxgl.Popup({ offset: [0, -15] })
-//             .setLngLat(e.lngLat)
-//             .setHTML(popupContent)
-//             .addTo(mapRef.current)
-//         }
-//       })
-//     })
-
-//     return () => mapRef.current.remove()
-//   }, [mapState])
-
-//   useEffect(() => {
-//     if (mapRef.current && mapRef.current.getSource('your-vector-source')) {
-//       mapRef.current
-//         .getSource('your-vector-source')
-//         .setData(`mapbox://${mapState.tilesetId}`)
-//       mapRef.current.flyTo({
-//         center: mapState.center,
-//         zoom: mapState.zoom,
-//       })
-//     }
-//   }, [mapState])
-
-//   return (
-//     <div className='map-container' style={{ width: '100%', height: '90vh' }}>
-//       <div>
-//         {tilesets.map(tileset => (
-//           <button
-//             key={tileset.id}
-//             onClick={() =>
-//               changeTileset(tileset.id, tileset.center, tileset.zoom)
-//             }
-//           >
-//             {tileset.id}
-//           </button>
-//         ))}
-//       </div>
-//       <div ref={mapContainerRef} style={{ width: '100%', height: '100%' }} />
-//     </div>
-//   )
-// }
-
-// export default App
